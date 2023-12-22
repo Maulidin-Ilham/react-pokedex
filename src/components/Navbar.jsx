7476e8c1460bb267a40d46858f979fc4bef2a814
Maulidin-Ilham <ilhammaulidinilham7373@gmail.com>
2023-12-22 10:03:28 +0700

    chore: Adding react icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,5 @@
+import { TfiGithub } from "react-icons/tfi";
+
 const Navbar = () => {
   return (
     <div className="flex flex-row justify-between items-center shadow-xl px-4 py-3">
@@ -5,7 +7,13 @@ const Navbar = () => {
         <img src="/assets/pokeball.png" alt="pokeball" className="w-10 h-10" />
         <h1 className="font-semibold text-lg">Pokedex</h1>
       </div>
-      <h1 className="font-semibold text-lg">Github</h1>
+      <a
+        href="https://github.com/Maulidin-Ilham/react-pokedex"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <TfiGithub />
+      </a>
     </div>
   );
 };
