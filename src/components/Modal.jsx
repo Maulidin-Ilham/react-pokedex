b7d7da3e85296021c4b89614223cc92759269e55
Maulidin-Ilham <ilhammaulidinilham7373@gmail.com>
2024-01-28 10:19:33 +0700

    feat: Add loading

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -4,16 +4,20 @@
 import { Dialog, Transition } from "@headlessui/react";
 import { Fragment, useEffect, useState } from "react";
 import { getPokemonDetails } from "../utils/api";
+import Loading from "./Loading";
 
 const Modal = ({ isOpen, onClose, selectedPokemon }) => {
   const [pokemonDetail, setPokemonDetail] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
+        setLoading(true);
         if (isOpen) {
           const data = await getPokemonDetails(selectedPokemon);
           setPokemonDetail(data);
+          setLoading(false);
         }
       } catch (error) {
         console.log(error);
@@ -42,82 +46,88 @@ const Modal = ({ isOpen, onClose, selectedPokemon }) => {
 
           <div className="fixed inset-0 overflow-y-auto">
             <div className="flex min-h-full items-center justify-center p-4 text-center">
-              <Transition.Child
-                as={Fragment}
-                enter="ease-out duration-300"
-                enterFrom="opacity-0 scale-95"
-                enterTo="opacity-100 scale-100"
-                leave="ease-in duration-200"
-                leaveFrom="opacity-100 scale-100"
-                leaveTo="opacity-0 scale-95"
-              >
-                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
-                  <Dialog.Title
-                    as="h3"
-                    className="text-lg font-semibold leading-6 text-gray-900 "
+              {!loading ? (
+                <>
+                  <Transition.Child
+                    as={Fragment}
+                    enter="ease-out duration-300"
+                    enterFrom="opacity-0 scale-95"
+                    enterTo="opacity-100 scale-100"
+                    leave="ease-in duration-200"
+                    leaveFrom="opacity-100 scale-100"
+                    leaveTo="opacity-0 scale-95"
                   >
-                    {pokemonDetail.name}
-                  </Dialog.Title>
-                  <div className="mt-2">
-                    <div className="flex flex-row ">
-                      {pokemonDetail.sprites &&
-                        pokemonDetail.sprites.front_default && (
-                          <img
-                            src={pokemonDetail.sprites.front_default}
-                            alt={pokemonDetail.name}
-                          />
-                        )}
+                    <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                      <Dialog.Title
+                        as="h3"
+                        className="text-lg font-semibold leading-6 text-gray-900 "
+                      >
+                        {pokemonDetail.name}
+                      </Dialog.Title>
+                      <div className="mt-2">
+                        <div className="flex flex-row ">
+                          {pokemonDetail.sprites &&
+                            pokemonDetail.sprites.front_default && (
+                              <img
+                                src={pokemonDetail.sprites.front_default}
+                                alt={pokemonDetail.name}
+                              />
+                            )}
 
-                      {pokemonDetail.sprites &&
-                        pokemonDetail.sprites.front_shiny && (
-                          <img
-                            src={pokemonDetail.sprites.front_shiny}
-                            alt={pokemonDetail.name}
-                          />
-                        )}
-                    </div>
-                    <div className="mt-2">
-                      <div>
-                        <h1 className="font-semibold">Abilities</h1>
-                      </div>
-                      <div className="mt-1">
-                        {pokemonDetail.abilities &&
-                          pokemonDetail.abilities.map((ability, index) => (
-                            <h1 key={index} className="capitalize">
-                              {ability.ability.name}
-                            </h1>
-                          ))}
-                      </div>
-                    </div>
+                          {pokemonDetail.sprites &&
+                            pokemonDetail.sprites.front_shiny && (
+                              <img
+                                src={pokemonDetail.sprites.front_shiny}
+                                alt={pokemonDetail.name}
+                              />
+                            )}
+                        </div>
+                        <div className="mt-2">
+                          <div>
+                            <h1 className="font-semibold">Abilities</h1>
+                          </div>
+                          <div className="mt-1">
+                            {pokemonDetail.abilities &&
+                              pokemonDetail.abilities.map((ability, index) => (
+                                <h1 key={index} className="capitalize">
+                                  {ability.ability.name}
+                                </h1>
+                              ))}
+                          </div>
+                        </div>
 
-                    <div className="mt-4">
-                      <div>
-                        <h1 className="font-semibold">Stats</h1>
+                        <div className="mt-4">
+                          <div>
+                            <h1 className="font-semibold">Stats</h1>
+                          </div>
+                          <div className="mt-1">
+                            {stats &&
+                              stats.map((stat, index) => (
+                                <div key={index}>
+                                  <h1>
+                                    {stat.stat.name} : {stat.base_stat}
+                                  </h1>
+                                </div>
+                              ))}
+                          </div>
+                        </div>
                       </div>
-                      <div className="mt-1">
-                        {stats &&
-                          stats.map((stat, index) => (
-                            <div key={index}>
-                              <h1>
-                                {stat.stat.name} : {stat.base_stat}
-                              </h1>
-                            </div>
-                          ))}
-                      </div>
-                    </div>
-                  </div>
 
-                  <div className="mt-4">
-                    <button
-                      type="button"
-                      className="inline-flex justify-center rounded border border-transparent bg-black text-white p-2 hover:opacity-80 transiton duration-150 ease-in-out font-semibold"
-                      onClick={onClose}
-                    >
-                      Got it, thanks!
-                    </button>
-                  </div>
-                </Dialog.Panel>
-              </Transition.Child>
+                      <div className="mt-4">
+                        <button
+                          type="button"
+                          className="inline-flex justify-center rounded border border-transparent bg-black text-white p-2 hover:opacity-80 transiton duration-150 ease-in-out font-semibold"
+                          onClick={onClose}
+                        >
+                          Got it, thanks!
+                        </button>
+                      </div>
+                    </Dialog.Panel>
+                  </Transition.Child>
+                </>
+              ) : (
+                <Loading />
+              )}
             </div>
           </div>
         </Dialog>
